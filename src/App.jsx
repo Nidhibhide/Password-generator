3e70313c7f9b234167689aa02390c0fa99e51299
Nidhibhide <nidhibhide144@gmail.com>
2025-04-16 09:10:23 +0530

    change path of folder

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-center text-blue-500 mt-10">Hey Good morning</h1>
+    </>
+  )
+}
+
+export default App
